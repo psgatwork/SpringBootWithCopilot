7462226a62b4eaa5fb13e1a929bc46379ef9306b
psgatwork <psgatwork@gmail.com>
2024-09-09 10:02:51 +0530

    Created Model Classes

---
+++ clinicalsapi/src/main/java/com/learninggenai/patientclinicals/clinicalsapi/clinicalsapi/models/ClinicalData.java
@@ -0,0 +1,82 @@
+package main.java.com.learninggenai.patientclinicals.clinicalsapi.clinicalsapi.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "clinicaldata")
+public class ClinicalData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String componentName;
+    private String componentValue;
+    private Timestamp measuredDateTime;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "patient_id", nullable = false)
+    @JsonIgnore
+    private Patient patient;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getComponentName() {
+        return componentName;
+    }
+
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
+
+    public String getComponentValue() {
+        return componentValue;
+    }
+
+    public void setComponentValue(String componentValue) {
+        this.componentValue = componentValue;
+    }
+
+    public Timestamp getMeasuredDateTime() {
+        return measuredDateTime;
+    }
+
+    public void setMeasuredDateTime(Timestamp measuredDateTime) {
+        this.measuredDateTime = measuredDateTime;
+    }
+
+    // Optional: Override toString, equals, and hashCode
+    @Override
+    public String toString() {
+        return "ClinicalData{" +
+                "id=" + id +
+                ", componentName='" + componentName + '\'' +
+                ", componentValue='" + componentValue + '\'' +
+                ", measuredDateTime=" + measuredDateTime +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ClinicalData that = (ClinicalData) o;
+
+        return id != null ? id.equals(that.id) : that.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
---
+++ clinicalsapi/src/main/java/com/learninggenai/patientclinicals/clinicalsapi/clinicalsapi/models/Patient.java
@@ -0,0 +1,78 @@
+package main.java.com.learninggenai.patientclinicals.clinicalsapi.clinicalsapi.models;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String firstName;
+    private String lastName;
+    private int age;
+    @OneToMany(mappedBy = "patient", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    private List<ClinicalData> clinicalData;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // Optional: Override toString, equals, and hashCode
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Patient patient = (Patient) o;
+
+        return id != null ? id.equals(patient.id) : patient.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
