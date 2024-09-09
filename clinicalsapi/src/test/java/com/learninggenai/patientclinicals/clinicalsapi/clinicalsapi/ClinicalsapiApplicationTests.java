066ac129785ab3d5e071f270854e5ca134227512
psgatwork <psgatwork@gmail.com>
2024-09-09 09:19:27 +0530

    Created New Project

---
+++ clinicalsapi/src/test/java/com/learninggenai/patientclinicals/clinicalsapi/clinicalsapi/ClinicalsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learninggenai.patientclinicals.clinicalsapi.clinicalsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicalsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
